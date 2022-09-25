939eac5e34e49365f6cfbb3c6ad773ccd59aa4ef
Nela Letochová <nela.letochova.wxc@gmail.com>
2022-09-25 08:23:40 +0200

    Vytvoření formuláře

--- src/components/Registration.jsx
+++ src/components/Registration.jsx
@@ -7,4 +7,25 @@ const Registration = () => {
     password: '',
     passwordConfirm: '',
   });
+
+  return (
+    <>
+      <form id="registration-form">
+        <h1>Registration</h1>
+        <input id="email_address" placeholder="Email Address" type="email" />
+        <input id="user_name" placeholder="User Name" type="text" />
+        <input id="password" placeholder="Password" type="password" />
+        <input
+          id="confirm_password"
+          placeholder="Confirm Password"
+          type="password"
+        />
+        <button id="registration-btn" type="submit">
+          Register
+        </button>
+      </form>
+    </>
+  );
 };
+
+export default Registration;
